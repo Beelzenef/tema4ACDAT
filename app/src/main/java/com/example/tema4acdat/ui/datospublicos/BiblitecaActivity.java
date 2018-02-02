34df9442df8c3890c0f4121b4f5fa9235f1108bb
Elena G <elena.guzbla@gmail.com>
2018-02-02 12:40:19 +0100

    fix viewBiblio, add email + url intents

--- app/src/main/java/com/example/tema4acdat/ui/datospublicos/BiblitecaActivity.java
+++ app/src/main/java/com/example/tema4acdat/ui/datospublicos/BiblitecaActivity.java
@@ -1,8 +1,12 @@
 package com.example.tema4acdat.ui.datospublicos;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.tema4acdat.R;
 import com.example.tema4acdat.pojo.Biblioteca;
@@ -21,24 +25,43 @@ public class BiblitecaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bibliteca);
 
-        getViews();
-        getBiblioteca();
-    }
-
-    private void getViews() {
         txtV_BiblioDescripcion = (TextView) findViewById(R.id.txtV_BiblioDescripcion);
         txtV_BiblioURL = (TextView) findViewById(R.id.txtV_BiblioURL);
         txtV_BiblioEmail = (TextView) findViewById(R.id.txtV_BiblioEmail);
         txtV_BiblioTelefono = (TextView) findViewById(R.id.txtV_BiblioTelefono);
-    }
 
-    private void getBiblioteca() {
         bibliotecaObservable = new Biblioteca();
-        bibliotecaObservable =  getIntent().getExtras().getBundle("Bundle").getParcelable("Biblio");
+        bibliotecaObservable = (Biblioteca) getIntent().getExtras().getBundle("Bundle").getParcelable("Biblio");
 
         txtV_BiblioDescripcion.setText(bibliotecaObservable.getDescripcion());
         txtV_BiblioEmail.setText(bibliotecaObservable.getEmail());
         txtV_BiblioTelefono.setText(bibliotecaObservable.getNumeroTlf());
         txtV_BiblioURL.setText(bibliotecaObservable.getURL());
+    }
+
+    public void onClick_URLBiblio(View view) {
+
+        switch (view.getId()) {
+            case R.id.txtV_BiblioURL:
+                Uri uri = Uri.parse(txtV_BiblioURL.getText().toString());
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                if (intent.resolveActivity(getPackageManager()) != null)
+                    startActivity(intent);
+                else
+                    Toast.makeText(getApplicationContext(), "No hay un navegador", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.txtV_BiblioEmail:
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, txtV_BiblioEmail.getText().toString());
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Asunto");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "Contenido");
+                if (emailIntent.resolveActivity(getPackageManager()) != null)
+                    startActivity(Intent.createChooser(emailIntent, "Enviar mensaje..."));
+                else
+                    Toast.makeText(getApplicationContext(), "No hay cliente de correo", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
